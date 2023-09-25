ddd93d238db564f392454051dfb2c634b71abb93
Juan <camejuanm@gmail.com>
2023-09-25 22:33:29 +0700

    added rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:"https://main--silver-kashata-a7ac2b.netlify.app",
+});
