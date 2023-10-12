0e95648917c865258d73a58264a1e517d9f21eb8
Juan <camejuanm@gmail.com>
2023-10-12 21:16:27 +0700

    added preact, light/darkmode and random greetings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    site:"https://main--silver-kashata-a7ac2b.netlify.app",
+  site: "https://main--silver-kashata-a7ac2b.netlify.app",
+  integrations: [preact()],
 });
