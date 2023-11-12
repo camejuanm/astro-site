24a1991d5c1a74e35bead84b62104be45a077fec
Juan <camejuanm@gmail.com>
2023-11-12 16:48:22 +0700

    bug fixes and version bump. 'astro:content' error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: [
+        '@astrojs/vue',
+        'astro-component-lib',
+      ]
+    }
+  },
   site: "https://main--silver-kashata-a7ac2b.netlify.app",
   integrations: [preact()],
 });
